867581175345c51441d9178fb5dbbdc1c2dd33d1
Alfred Lewis <alewis1719@gmail.com>
2021-04-26 21:52:13 -0500

    [i-9] Add post reducer tests

---
+++ src/store/mockData.ts
@@ -0,0 +1,54 @@
+export const posts = [
+	{
+		id: 'mz0rp8',
+		title: 'Pretty much.',
+		thumbnail:
+			'https://b.thumbs.redditmedia.com/ZWctFp4AW0LPwuT6oF6V_9Vf5t_G7VypaXZxg_z-qIA.jpg',
+		image:
+			'https://preview.redd.it/3pasxhrchjv61.jpg?auto=webp&s=a1e43a94d4981d31f0bf0ff301766ee117f885df',
+		author: 'Wamges',
+		time: 1619452219,
+		comments: 1285,
+		viewed: true,
+		inGallery: false
+	},
+	{
+		id: 'mz08yr',
+		title: 'He plays bad, but no so bad',
+		thumbnail:
+			'https://b.thumbs.redditmedia.com/kcqEk2iG0YBLCwkcSeJDQssonGsmL2Gt33B64b_kC5k.jpg',
+		image:
+			'https://external-preview.redd.it/05ymX9XzklsRS_u0AINMNO2qCQJJF2yOGCikIT71r5Q.png?format=pjpg&auto=webp&s=cf7cba2e0ad30a0ec5ae74ae35adbe4141461c0a',
+		author: 'Szorzs',
+		time: 1619450838,
+		comments: 838,
+		viewed: true,
+		inGallery: true
+	},
+	{
+		id: 'myy6uw',
+		title: 'Maybe I skipped that chapter in school',
+		thumbnail:
+			'https://b.thumbs.redditmedia.com/v_pnZwavIiQDx5CggdjI-rFnqKH_I_hOVZ9-DI0A91w.jpg',
+		image:
+			'https://preview.redd.it/lpdosmnnwiv61.jpg?auto=webp&s=71c45c050a482af0e3f53674af4dd3c0402b0a7d',
+		author: '__Dawn__Amber__',
+		time: 1619445250,
+		comments: 283,
+		viewed: true,
+		inGallery: false
+	},
+	{
+		id: 'mymnig',
+		title: 'This 9 years old ability to skateboard!',
+		thumbnail:
+			'https://b.thumbs.redditmedia.com/Ri2o6FnmXF7a3QNc8yeyfVuCvZ2LFXsQ8m1uWpmPVwM.jpg',
+		image:
+			'https://external-preview.redd.it/u6X6g1k_rtX36iPKbfePY0YUnUm03wRaToxT1wkEJnk.png?format=pjpg&auto=webp&s=9f652720c82e095a6cda2931dbf13252bcea3095',
+		author: 'myownfantasyworld',
+		time: 1619400862,
+		comments: 1499,
+		viewed: true,
+		inGallery: true
+	}
+];
